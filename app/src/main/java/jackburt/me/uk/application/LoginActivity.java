be591583d688d9341c361f1eac817e94a095cc9b
DESKTOP-A5VNKE5\jackb <jack.burt1@hotmail.co.uk>
2019-11-09 01:22:45 +0000

    Migrated to AndroidX and upgraded gradel and other things

--- app/src/main/java/jackburt/me/uk/application/LoginActivity.java
+++ app/src/main/java/jackburt/me/uk/application/LoginActivity.java
@@ -1,6 +1,6 @@
 package jackburt.me.uk.application;
 
-import android.support.v7.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class LoginActivity extends AppCompatActivity {
